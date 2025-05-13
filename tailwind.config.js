39db394925b977b7e0a2fce23a5e0505d4ac25f6
sohelx196 <sohel.devloper196@gmail.com>
2025-05-13 10:32:52 +0530

    working...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
  /** @type {import('tailwindcss').Config} */
- export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {
+       fontFamily: {
       inter: ['Inter', 'sans-serif'],
       cabin: ['Cabin', 'sans-serif'],
       poppins: ['Poppins', 'sans-serif'],
     },
-  },
-  },
-  plugins: [],
-}
+     },
+   },
+   plugins: [],
+ }
